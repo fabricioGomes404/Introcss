9719834276e1fbe60b9b01d8079e261e07cb856b
fabricioGomes404 <fabricio.pereira18@aluno.ce.gov.br>
2026-03-10 12:46:15 -0300

    dtjntj

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+let Nome = document.getElementById("Nome")
+let Email = document.getElementById("Email")
+let CPF = document.getElementById("CPF")
+let Telefone = document.getElementById("Telefone")
+let p_nome = document.getElementById("p_nome")
+let p_email = document.getElementById("p_email")
+let p_cpf = document.getElementById("p_cpf")
+let p_tel = document.getElementById("p_tel")
+
+if (Nome == " ") {
+    p_nome
+}
